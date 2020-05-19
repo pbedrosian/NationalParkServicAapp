1f7c999308cac2b02bd7736543bf2b9c4d5cb9d7
pbedrosian <peter_bedrosian@Yahoo.com>
2020-05-18 22:32:38 -0400

    Added User accounts validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,21 +18,34 @@ class UsersController < ApplicationController
     erb :'/users/signup.html'
   end
 
+  # post '/signup' do
+  #   if params[:email] == "" || params[:password] == ""
+  #     erb :'/failures/signup_missing_info.html' #needs at least an email and password
+  #   elsif Helpers.get_email(params)
+  #     erb :'/failures/signup_email_exists.html' #email already taken
+  #   elsif !Helpers.confirm_password(params)
+  #     erb :'/failures/signup_pass_missmatch.html'
+  #   else
+  #     @user = User.new(email: params[:email], 
+  #     password: params[:password],
+  #     first_name: params[:first_name],
+  #     last_name: params[:last_name])
+  #     @user.save
+  #     session[:user_id] = @user.id
+  #     redirect '/parks'
+  #   end
+  # end
+
   post '/signup' do
-    if params[:email] == "" || params[:password] == ""
-      erb :'/failures/signup_missing_info.html' #needs at least an email and password
-    elsif Helpers.get_email(params)
-      erb :'/failures/signup_email_exists.html' #email already taken
-    elsif !Helpers.confirm_password(params)
-      erb :'/failures/signup_pass_missmatch.html'
-    else
       @user = User.new(email: params[:email], 
       password: params[:password],
       first_name: params[:first_name],
       last_name: params[:last_name])
-      @user.save
-      session[:user_id] = @user.id
-      redirect '/parks'
+      if @user.save
+        session[:user_id] = @user.id
+        redirect '/parks'
+      else
+        erb :'/users/signup.error.html'
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :parks
-    validates :name, presence: true
+    validates :password, :presence => true,
+                         :confirmation => true,
+                         :length => {:within => 6..40},
+                         :on => :create
+
+
+    validates :password, :presence => true,
+                         :confirmation => true,
+                         :length => {:within => 6..40},
+                         :on => :update
+                         
+    validates :email, uniqueness: true
 end
